3fa8cee13cca0b95926938da64e90cf357b746bf
Arsbul-hub <ars.bulygin@yandex.ru>
2024-06-05 21:04:15 +0300

    Initial commit

---
+++ app/src/main/java/com/arsbulapps/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.arsbulapps.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.arsbulapps.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
